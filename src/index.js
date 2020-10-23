2b62fa3bbb69a39f549421074925c801c79293fe
Umidullo Suyunov <suyunovumidullo@gmail.com>
2020-10-24 02:09:37 +0500

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,25 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+   
+  const splitted_expr = expr.match(/.{1,10}/g);
+
+  const splitted_nums = splitted_expr.map((letter) => letter.match(/.{1,2}/g));
+
+  const morse_letters = splitted_nums.map((nums) =>
+    nums
+      .map((num) =>
+        num === '**' ? num : +num === 10 ? '.' : +num == 11 ? '-' : ''
+      )
+      .join('')
+  );
+  const decoded = morse_letters.map((ltr) => MORSE_TABLE[ltr]).join('');
+
+  return decoded;
 }
 
 module.exports = {
